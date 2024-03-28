33212adcd0cf83575298fa970cd4827d2d276785
Catalina Krenz <caty_kr_enz@hotmail.com>
2024-03-27 23:41:59 -0300

    segunda practica

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  first_name: {
+    type: String,
+    //required: true,
+  },
+  last_name: {
+    type: String,
+    //required: true,
+  },
+  email: {
+    type: String,
+    //required: true,
+    unique: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    //required: true,
+  },
+  age: {
+    type: Number,
+    //required: true,
+  },
+  rol: {
+    type: String,
+    enum: ["admin", "user"], //Enumera los roles permitidos
+    default: "user", //Asignamos por default "user"
+  },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+
+module.exports = UserModel;
